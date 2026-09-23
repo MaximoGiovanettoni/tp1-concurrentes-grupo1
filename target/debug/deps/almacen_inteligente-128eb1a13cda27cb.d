c789a903e23bd12e3764acfdcaa8eff8688c113d
Maximo Giovanettoni <mgiovanettoni@fi.uba.ar>
2026-09-23 10:23:43 -0300

    Add incremental build artifacts for almacen_inteligente
    - Created new work-products.bin file to store build metadata.
    - Added lock file for incremental build management.

---
+++ target/debug/deps/almacen_inteligente-128eb1a13cda27cb.d
@@ -0,0 +1,11 @@
+/home/machi/AA_FIUBA/Concu/tp1-concurrentes-grupo1/target/debug/deps/libalmacen_inteligente-128eb1a13cda27cb.rmeta: src/lib.rs src/mantenimiento.rs src/recepcion.rs Cargo.toml
+
+/home/machi/AA_FIUBA/Concu/tp1-concurrentes-grupo1/target/debug/deps/almacen_inteligente-128eb1a13cda27cb.d: src/lib.rs src/mantenimiento.rs src/recepcion.rs Cargo.toml
+
+src/lib.rs:
+src/mantenimiento.rs:
+src/recepcion.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/almacen_inteligente-d94ee99934918d17.d
@@ -0,0 +1,9 @@
+/home/machi/AA_FIUBA/Concu/tp1-concurrentes-grupo1/target/debug/deps/libalmacen_inteligente-d94ee99934918d17.rmeta: src/main.rs Cargo.toml
+
+/home/machi/AA_FIUBA/Concu/tp1-concurrentes-grupo1/target/debug/deps/almacen_inteligente-d94ee99934918d17.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
