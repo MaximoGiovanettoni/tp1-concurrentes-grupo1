3d2bba0f08d0d61f18a412900942219829c2d1d7
Maximo Giovanettoni <mgiovanettoni@fi.uba.ar>
2026-09-23 10:16:25 -0300

    Add settings management and sidebar functionality
    - Introduced a new JavaScript file for managing user settings on the settings page, allowing users to change themes and other preferences.
    - Implemented a sidebar for navigating source files, enhancing user experience on mobile devices.
    - Added functionality to highlight specific lines in source code based on URL hash.
    - Created a new SVG icon for visual representation in the UI.
    - Enhanced local storage management for persisting user preferences across sessions.

---
+++ target/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"almacen_inteligente":{"doc":"","t":"AAFF","n":["mantenimiento","recepcion","ejecutar","ejecutar"],"q":[[0,"almacen_inteligente"],[2,"almacen_inteligente::mantenimiento"],[3,"almacen_inteligente::recepcion"]],"d":["","","",""],"i":[0,0,0,0],"f":[0,0,[[],1],[[],1]],"c":[],"p":[[15,"tuple"]],"b":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ target/doc/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"almacen_inteligente":["",[],["lib.rs","mantenimiento.rs","recepcion.rs"]]\
+}');
+createSrcSidebar();
